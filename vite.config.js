584b9c00e2a2f0dc50bb41a5bba77cb179adec89
Alex Lévy <alex.levy@relet-digital.com>
2025-02-17 20:48:34 +0100

    Add base path configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
-}) 
+	server: {
+		port: 3000,
+		open: true,
+	},
+	build: {
+		outDir: "dist",
+		assetsDir: "assets",
+	},
+	base: "/fdssv2",
+})
